b8256c715975bf3b2a456c0ed5ff3f4fc59d921e
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-11-16 17:02:00 -0800

    set up goal's model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :create, :new]
+  resources :users, only: [:show, :update, :delete, :create]
+  resources :goals, only: [:show, :create, :new]
   resource :session, only: [:destroy, :create, :new]
+  
+  
 end
